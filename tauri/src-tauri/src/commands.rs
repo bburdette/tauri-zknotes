48f2b52d19f7cad06801e3d09de8779924c63bc7
Ben Burdette <>
2024-12-28 09:20:34 -0700

    experimenting with actix_rt version of girlboss

--- tauri/src-tauri/src/commands.rs
+++ tauri/src-tauri/src/commands.rs
@@ -197,6 +197,55 @@ pub fn fileresp_helper(
   }
 }
 
+// #[tauri::command]
+// pub fn zimsg(state: State<ZkState>, msg: PrivateMessage) -> PrivateTimedData {
+//   // gonna need config obj, uid.
+//   // uid could be passed from elm maybe.
+
+//   println!("zimsg");
+
+//   let stateclone = state.state.clone();
+
+//   // let res = tauri::async_runtime::block_on(async move {
+//   let res = std::thread::spawn(move || {
+//     let rt = actix_rt::System::new();
+//     let state = stateclone.write().unwrap();
+//     // let serv = atomic_server_lib::serve::serve(config_clone);
+//     let zkres = zknotes_server_lib::interfaces::zk_interface_loggedin(&state, 2, &msg);
+//     match (
+//       rt.block_on(zkres),
+//       SystemTime::now()
+//         .duration_since(SystemTime::UNIX_EPOCH)
+//         .map(|n| n.as_millis()),
+//     ) {
+//       (Ok(sr), Ok(t)) => {
+//         println!("sr: {:?}", sr.what);
+//         // serde_json::to_value(&sr).unwrap());
+//         PrivateTimedData {
+//           utcmillis: t,
+//           data: sr,
+//         }
+//       }
+//       (Err(e), _) => PrivateTimedData {
+//         utcmillis: 0,
+//         data: PrivateReplyMessage {
+//           what: PrivateReplies::ServerError,
+//           content: Value::String(e.to_string()),
+//         },
+//       },
+//       (_, Err(e)) => PrivateTimedData {
+//         utcmillis: 0,
+//         data: PrivateReplyMessage {
+//           what: PrivateReplies::ServerError,
+//           content: Value::String(e.to_string()),
+//         },
+//       },
+//     }
+//   });
+
+//   res.join().unwrap()
+// }
+
 #[tauri::command]
 pub fn zimsg(state: State<'_, ZkState>, msg: PrivateMessage) -> PrivateTimedData {
   println!("zimsg");
@@ -214,7 +263,7 @@ pub fn zimsg(state: State<'_, ZkState>, msg: PrivateMessage) -> PrivateTimedData
 }
 
 pub fn zimsg_err(
-  state: State<'_, ZkState>,
+  state: State<ZkState>,
   msg: PrivateMessage,
 ) -> Result<PrivateTimedData, zkerr::Error> {
   let conn = sqldata::connection_open(
@@ -230,11 +279,25 @@ pub fn zimsg_err(
   let uid =
     get_tauri_uid(&conn)?.ok_or(zkerr::Error::String("zimsg: not logged in".to_string()))?;
 
-  let sr = tauri::async_runtime::block_on(zknotes_server_lib::interfaces::zk_interface_loggedin(
-    &state.state.read().unwrap(), // TODO fix
-    uid,
-    &msg,
-  ));
+  // let sr = tauri::async_runtime::block_on(zknotes_server_lib::interfaces::zk_interface_loggedin(
+  //   &state.state.read().unwrap(), // TODO fix
+  //   uid,
+  //   &msg,
+  // ));
+
+  let sr = actix_rt::Runtime::new().unwrap().block_on(
+    zknotes_server_lib::interfaces::zk_interface_loggedin(
+      &state.state.read().unwrap(), // TODO fix
+      uid,
+      &msg,
+    ),
+  );
+
+  // let sr = tauri::async_runtime::block_on(zknotes_server_lib::interfaces::zk_interface_loggedin(
+  //   &state.state.read().unwrap(), // TODO fix
+  //   uid,
+  //   &msg,
+  // ));
 
   let dt = sr?;
 
